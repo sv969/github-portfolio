ac80a5882360483b4b38accb222fea7ae058e79c
Sampath Sai Varukolu <166189909+sv969@users.noreply.github.com>
2025-06-01 20:37:30 -0400

    Update main.js
    new jersey lat and lng chnaged

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,7 +141,7 @@ function validateEmail(email) {
 /*===== GOOGLE MAPS =====*/
 var mapProp;
 function myMap() {
-  var latLng = new google.maps.LatLng(35.198284, -111.651299);
+  var latLng = new google.maps.LatLng(40.07304, -74.724325);
   mapProp = {
     center: latLng,
     zoom: 6,
